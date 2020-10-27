60538d46df52f0ea46b9fa20bed5d705fad94793
GilCristianGama <ppgama50@gmail.com>
2020-10-27 10:37:06 -0300

    first commit

---
+++ Index.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="pt-Br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Bootrapa -->
+    <link rel="stylesheet" href="public/Css/Bootstrap/css/bootstrap.min.css">
+
+
+    <!-- MEU STILO -->
+    <link rel="stylesheet" href="public/Css/meuStilo.css">
+
+    <!-- Icomoon -->
+    <link rel="stylesheet" href="public/fontes/icomomun.css">
+
+    <title>Meu Site</title>
+
+
+
+</head>
+
+<body>
+
+
+
+    <!-- containner inicio-->
+
+    <div class="container">
+        <div class="borda radius ">
+
+            <div class="card-body">
+
+                <header class="alert-danger">
+
+                    <div class="row">
+                        <div class="col-3">
+                            <img src="public/Img/Face_n.jpg" class="img-fluid img-thumbnail" alt="Destaque do produto"
+                                alt="Imagem do produto">
+                        </div>
+                        <div class="col-9">
+                            <h2 class="texte-center ">Gil Cristian
+                            </h2>
+                            <p>Constante no trabalho,aprendendo sempre cada dia esta disponivel pra tudo ,sem medo de
+                                errar.</p>
+                            <hr>
+                            <p><i class="chris-google"></i>ppgama50@gmail.com</p>
+                            <p><i class="chris-facebook2"></i> ga_gilcris@hotmail.com</p>
+                            <p><i class="chris-twitter"></i>@PpgamaC</p>
+                            <p><i class="chris-whatsapp"></i>(81)98134-0913</p>
+                        </div>
+                    </div>
+                </header>
+                <!-- ********************************
+                -->
+                <h1> Empresas</h1>
+
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div id="list-example" class="list-group">
+                                <a class="list-group-item list-group-item-action" href="#list-item-1"> <i
+                                        class="chris-star-full"> </i><strong>C&A </strong></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-2"> <i
+                                        class="chris-star-full"> </i><strong> REAL HOSPITAL PORTUGUÊS</strong></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-3"> <i
+                                        class="chris-star-full"> </i><strong>LEÃO ARTIGOS
+                                    </strong></a>
+                                <a class="list-group-item list-group-item-action" href="#list-item-4"> <i
+                                        class="chris-clipboard"> </i> <strong>CURSOS </strong></a>
+
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div data-spy="scroll" data-target="#list-example" data-offset="0"
+                                class="scrollspy-example scrollspyProdutos">
+                                <h4 id="list-item-1">C&A</h4>
+                                <p>C&A é uma cadeia internacional de lojas de vestuário. No Brasil, é uma das maiores
+                                    rede de
+                                    lojas de
+                                    departamento do país e a décima segunda maior empresa varejista, segundo ranking do
+                                    Ibevar
+                                    em 2012. Foi
+                                    fundada nos Países Baixos em 1841 pelos irmãos Clemens e August como uma empresa
+                                    têxtil</p>
+                                <h4 id="list-item-2">Real Hospital Português</h4>
+                                <p>O Real Hospital Português é um hospital brasileiro, fundado em 16 de novembro de 1855
+                                    na
+                                    cidade do
+                                    Recife,
+                                    capital de Pernambuco. Trata-se do maior complexo hospitalar do Norte-Nordeste do
+                                    Brasil.
+                                </p>
+                                <h4 id="list-item-3">Leão Artigos</h4>
+                                <p>localizada na rua santa Rita no Bairro de Sao jose ,trabalhando com objetos
+                                    importados da
+                                    china.</p>
+
+                                <h4 id="list-item-4">SENAC</h4>
+                                <p>Web Design duracao 120h </p>
+                                <h4 id="list-item-5">SECURYTY CENTER</h4>
+                                <p>Dotar o aluno de conhecimentos, técnicas, habilidades e atitudes que o capacitem para
+                                    o exercício da profissão de vigilante, em complemento à segurança pública, incluídas
+                                    as atividades relativas à vigilância patrimonial, à segurança física de
+                                    estabelecimentos financeiros e outros, preparo para dar atendimento e segurança às
+                                    pessoas e manutenção da integridade do patrimônio que guarda,
+                                    bem como adestramento para o uso de armamento convencional e o emprego de defesa
+                                    pessoal;</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- fim da container -->
+
+            <!-- text Sobre -->
+            <hr>
+            <div class="text-center alert-dark" role="alert">
+                <h4 class="alert-heading">Sobre min!</h4>
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolores voluptatum laborum, iste adipisci
+                    nesciunt consectetur.
+                    Velit animi exercitationem aspernatur totam deleniti recusandae alias non dolor numquam eaque,
+                    voluptate, ea vel.</p>
+                <hr>
+                <p class="mb-0">Sempre que precisar, use utilitários de margem para manter as coisas perfeitas.</p>
+            </div>
+
+        </div>
+
+    </div>
+
+    
+    <!-- Footer -->
+
+    <hr>
+    <footer class="container-fluid table-dark">
+        <div class="container">
+
+
+            <h1 class="text-right"><i class="chris-facebook"></i>
+                <i class="chris-twitter"></i>
+                <i class="chris-dribbble"></i>
+            </h1>
+        </div>
+        <!-- Fim container -->
+    </footer>
+    <script src="pulic/js/jquery.js"></script>
+
+    <script src="public/Css/Bootstrap/js/bootstrap.min.js"></script>
+
+    <!--  -->
+
+</body>
+
+</html>
